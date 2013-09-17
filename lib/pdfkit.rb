4c212b2d280ab11e2b4085015fcc5dd0ad70eab0
jaymor <jayesh@rapidops.com>
2013-09-17 15:08:21 -0400

    Fake comment to test commit

--- lib/pdfkit.rb
+++ lib/pdfkit.rb
@@ -1,4 +1,6 @@
+# adding a fake comment to trigger change
 require 'pdfkit/source'
 require 'pdfkit/pdfkit'
 require 'pdfkit/middleware'
+# this is again a fake comment.
 require 'pdfkit/configuration'
